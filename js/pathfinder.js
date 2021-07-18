d72ef4f9a690908597aa1b5b2b18b1aabb7b1cc2
Andrea Cacioli <andrea.cacioli@edu.unito.it>
2021-07-18 14:33:00 +0200

    Added Logo and pathfinder page

---
+++ js/pathfinder.js
@@ -0,0 +1,9 @@
+var button = document.getElementById('OkButton');
+
+button.addEventListener('click', function(){
+    var rows = document.getElementById('RowInput');
+    var columns = document.getElementById('ColumnInput');
+
+    console.log(rows);
+    console.log(columns);
+});
