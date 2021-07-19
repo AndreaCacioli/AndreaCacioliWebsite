afffc29e4834ce613c85c5b0567dd08b67715a3b
Andrea Cacioli <andrea.cacioli@edu.unito.it>
2021-07-19 16:28:14 +0200

    🖌️Pathfinder restiling🎨

--- js/pathfinder.js
+++ js/pathfinder.js
@@ -1,9 +1,35 @@
-var button = document.getElementById('OkButton');
+var canvas
+var button
+var slider
 
-button.addEventListener('click', function(){
-    var rows = document.getElementById('RowInput');
-    var columns = document.getElementById('ColumnInput');
+function setup()
+{
+    canvas = createCanvas(windowWidth - 10, windowHeight- 10)
+    canvas.position(5,5)
 
-    console.log(rows);
-    console.log(columns);
-});
+    button = createButton('OK!')
+    button.position((width - 100), height/20)
+    button.addClass('btn')
+
+   
+   slider = createSlider(10, 500, 50);
+   slider.position(10, 10);
+   slider.style('width', '200px');
+   slider.position(200, height/20);
+   slider.input(function(){
+       console.log(slider.value())
+   })
+
+}
+
+function draw()
+{
+    background(90)
+}
+
+function windowResized()
+{
+    resizeCanvas(windowWidth - 10, windowHeight - 10)
+    canvas.position(5,5)
+    button.position((width - 100), height/20)
+}
